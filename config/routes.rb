98ac69e4059c6a3d03f19fe46edee178616e6547
jc <emclab2011@gmail.com>
2014-06-13 16:18:12 -0500

    JC:changed from put to get for search_results in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,25 @@ OnboardDatax::Engine.routes.draw do
   resources :onboard_engine_configs do
     collection do
       get :search
-      put :search_results  
+      get :search_results  
     end
   end
   resources :onboard_engine_inits do
     collection do
       get :search
-      put :search_results  
+      get :search_results  
     end
   end
   resources :onboard_user_accesses do
     collection do
       get :search
-      put :search_results  
+      get :search_results  
     end
   end
   resources :onboard_search_stat_configs do
     collection do
       get :search
-      put :search_results  
+      get :search_results  
     end
   end
 
